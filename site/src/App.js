c4399d2e769e8ea4cb0ad3e73f892704277dc4b3
Vincent Occhiogrosso <vocchiogrosso@users.noreply.github.com>
2018-09-15 04:35:28 -0400

    #1

---
+++ site/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+document.title="Home";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
