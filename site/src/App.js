f85771aaa22b03804954309c98927787d138ad5f
Vincent Occhiogrosso <vocchiogrosso@users.noreply.github.com>
2018-09-15 14:34:25 -0400

    #4
    + Added Zip

--- site/src/App.js
+++ site/src/App.js
@@ -28,26 +28,28 @@ class App extends Component {
         </div>
         <div className="Screen" id="B">
           <h2 class="HeadText WhiteText">Demo</h2>
-        
+          {/*Link To Demo Project*/}
+          <a href="http://www.google.com"><h1 id="DL">Download SolVR for PC</h1></a>
         </div>
         <div className="Screen" id="C">
           <h2 class="HeadText">About</h2>
-
+          <p id="EL">Our Project Is Designed To Help Individuals Improve Visual Coordination.</p>
+          <p id="EL">All Of This Has Been Built In A State Of The Art Virtual Enviorment</p>
         </div>
         <div className="Screen" id="D">
           <h2 class="HeadText WhiteText">Our Team</h2>
-          <div class="PersonalPanel" id="PPA"><img class="Photo" src={Vin}/><p class="Name">Vincent Occhiogrosso</p><p id="SP">React Developer</p>    <a target="_blank" href="https://github.com/vocchiogrosso"><div className='PERSONALLINK'><p id="PPP">Visit</p></div></a>  </div>
-          <div class="PersonalPanel" id="PPB"><img class="Photo" src={Vic}/><p class="Name">Victor Lin</p><p id="SP">Python/Java</p>                  <a target="_blank" href="https://github.com/AllThingsSlippy"><div className='PERSONALLINK'><p id="PPP">Visit</p></div></a>  </div>
-          <div class="PersonalPanel" id="PPC"><img class="Photo" src={Spe}/><p class="Name">Spencer Williams</p><p id="SP">C++/Unreal</p>             <a target="_blank" href="https://github.com/spencer-williams"><div className='PERSONALLINK'><p id="PPP">Visit</p></div></a>  </div>
-          <div class="PersonalPanel" id="PPD"><img class="Photo" src={Jer}/><p class="Name">Jerry Turcios</p><p id="SP">Everything</p>                <a target="_blank" href="https://github.com/jerryturcios08"><div className='PERSONALLINK'><p id="PPP">Visit</p></div></a>  </div>
-          <div class="PersonalPanel" id="PPE"><img class="Photo" src={Man}/><p class="Name">Manuel Carbajal</p><p id="SP">C++/Java</p>                <a target="_blank" href="https://github.com/MCarbajalSuarez"><div className='PERSONALLINK'><p id="PPP">Visit</p></div></a>  </div>
+          <div class="PersonalPanel" id="PPA"><img alt="" class="Photo" src={Vin}/><p class="Name">Vincent Occhiogrosso</p><p id="SP">React Developer</p>    <a target="_blank" rel="noopener noreferrer" href="https://github.com/vocchiogrosso"><div className='PERSONALLINK'><p id="PPP">Visit</p></div></a>  </div>
+          <div class="PersonalPanel" id="PPB"><img alt="" class="Photo" src={Vic}/><p class="Name">Victor Lin</p><p id="SP">Python/Java</p>                  <a target="_blank" rel="noopener noreferrer"  href="https://github.com/AllThingsSlippy"><div className='PERSONALLINK'><p id="PPP">Visit</p></div></a>  </div>
+          <div class="PersonalPanel" id="PPC"><img alt="" class="Photo" src={Spe}/><p class="Name">Spencer Williams</p><p id="SP">C++/Unreal</p>             <a target="_blank"  rel="noopener noreferrer"href="https://github.com/spencer-williams"><div className='PERSONALLINK'><p id="PPP">Visit</p></div></a>  </div>
+          <div class="PersonalPanel" id="PPD"><img alt="" class="Photo" src={Jer}/><p class="Name">Jerry Turcios</p><p id="SP">Everything</p>                <a target="_blank" rel="noopener noreferrer" href="https://github.com/jerryturcios08"><div className='PERSONALLINK'><p id="PPP">Visit</p></div></a>  </div>
+          <div class="PersonalPanel" id="PPE"><img alt="" class="Photo" src={Man}/><p class="Name">Manuel Carbajal</p><p id="SP">C++/Java</p>                <a target="_blank" rel="noopener noreferrer" href="https://github.com/MCarbajalSuarez"><div className='PERSONALLINK'><p id="PPP">Visit</p></div></a>  </div>
         </div>
         <div className="Screen" id="E">
             <div className="SubScreen">
               <h2 class="HeadText">Follow Us</h2>
-              <a target="_blank" href="https://www.facebook.com/Solvr-240537483471698/?view_public_for=240537483471698" target="_blank"><div class="SocialLink" id="SL1"></div></a>
-              <a target="_blank" href="https://twitter.com/Solvr12"><div class="SocialLink" id="SL2"></div></a>
-              <a target="_blank" href="https://github.com/Sol-VR"><div class="SocialLink" id="SL3"></div></a>
+              <a target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/Solvr-240537483471698/?view_public_for=240537483471698"><div class="SocialLink" id="SL1"></div></a>
+              <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/Solvr12"><div class="SocialLink" id="SL2"></div></a>
+              <a target="_blank" rel="noopener noreferrer" href="https://github.com/Sol-VR"><div class="SocialLink" id="SL3"></div></a>
             </div>
             <div className="Bar" id="Footer">
               <p>Made With <span class="R">&hearts;</span> By SolVR (C) 2018</p>
